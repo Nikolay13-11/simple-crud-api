04611dadd386d8ec1692e7f63b215df08bf7cbcc
Nikolay13-11 <mikola13.11@mail.ru>
2021-11-26 01:00:43 +0300

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 
 module.exports = {
-  entry: path.resolve(__dirname, 'server.js'),
+  entry: path.resolve(__dirname, './dist/bundle.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
