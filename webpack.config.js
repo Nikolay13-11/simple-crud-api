e9f27a5b585c5090995ae87cb4c10a161d8a8549
Nikolay13-11 <mikola13.11@mail.ru>
2021-11-27 17:21:17 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,13 @@
 const path = require('path');
 
-
 module.exports = {
-  entry: path.resolve(__dirname, './dist/bundle.js'),
+  entry: './server.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  mode: 'production',
   target: 'node',
-  module: {
-    rules: [
-    {test: /\.txt$/, use: 'raw-loader'}
-    ]
-    },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
 };
